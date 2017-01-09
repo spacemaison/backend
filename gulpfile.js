ccf47d470603b3efc81728ce7bcb97ea9f4b73cb
Jesse Tuchsen <tuchsen@hush.ai>
2017-01-09 15:04:53 -0800

    Added linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp')
-const gutil = require('gulp-util')
+const standard = require('gulp-standard')
 const babel = require('gulp-babel')
 
-gulp.task('backend', () =>
-  gulp.src('src/**/*.js')
+const SRC_FILES = [
+  'src/**/*.js'
+]
+
+gulp.task('lint', () =>
+  gulp.src(SRC_FILES)
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true
+    }))
+)
+
+gulp.task('backend', [ 'lint' ], () =>
+  gulp.src(SRC_FILES)
     .pipe(babel({
-      plugins: [ "transform-es2015-modules-commonjs" ]
+      plugins: [ 'transform-es2015-modules-commonjs' ]
     }))
     .pipe(gulp.dest('out'))
 )
